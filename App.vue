9be7e8c94413ec65e28fdba4dc645d839fa241b1
鹏超 刘 <876367352@qq.com>
2022-12-23 11:10:25 +0800

    创建玩android小程序项目

---
+++ App.vue
@@ -0,0 +1,19 @@
+
+<!-- 应用配置，用来配置小程序的全局样式、生命周期函数等 -->
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+</style>
